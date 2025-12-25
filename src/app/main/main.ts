80dcb166741ceee0725e4ec939cba1848b7cfcef
Tarun K. Gupta <tg.github@gmail.com>
2025-12-25 17:09:18 +0530

    skill section added

--- src/app/main/main.ts
+++ src/app/main/main.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
 import { Header } from '../common/header/header';
 import { Footer } from '../common/footer/footer';
 import { RouterModule } from '@angular/router';
@@ -8,5 +13,7 @@ import { RouterModule } from '@angular/router';
   imports: [Header, Footer, RouterModule],
   templateUrl: './main.html',
   styleUrl: './main.css',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Main {}
